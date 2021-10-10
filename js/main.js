f55c6f40c84a7ef5596f942a03cdad39e374fafb
Personal Profile <davina.leong.sy@gmail.com>
2021-10-10 08:36:50 +0800

    Implemented append projects function

--- js/main.js
+++ js/main.js
@@ -14,12 +14,16 @@ function goToViewProjectPage() {
   goToPage('projects-view.html');
 }
 
-const projectCardHtml = '<div class="card"><img src="images/site.png" class="card-img-top" alt="Site Name Screenshot"><div class="card-body"><h5 class="card-title">Site Name</h5><a href="cpanels-view.html" class="btn btn-primary">View Project <i class="fas fa-eye fa-fw"></i></a></div></div>';
+var projectCardHtml = `<div class="card">
+  <img src="images/site.png" class="card-img-top" alt="Site Name Screenshot">
+  <div class="card-body">
+    <h5 class="card-title">Site Name</h5>
+    <a href="cpanels-view.html" class="btn btn-primary">View Project <i class="fas fa-eye fa-fw"></i></a>
+  </div>
+</div>`;
 
-function appendProjects(count, elementId) {
-  var projectCardsHtml = '';
-  for (let i = 0; i < count; i++) {
-    projectCardsHtml += projectCardHtml;
+function appendProjects(count, selector) {
+  for (var i = 0; i < count; i++) {
+    $(selector).append(projectCardHtml);
   }
-  document.getElementById(elementId).innerHTML(projectCardsHtml);
 }
