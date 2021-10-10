38602471cca8b07ebe8a1353b5d76f65181c9354
Personal Profile <davina.leong.sy@gmail.com>
2021-10-10 08:27:09 +0800

    Installed jquery; WIP append projects

--- js/main.js
+++ js/main.js
@@ -13,3 +13,13 @@ function goToViewCpanelPage() {
 function goToViewProjectPage() {
   goToPage('projects-view.html');
 }
+
+const projectCardHtml = '<div class="card"><img src="images/site.png" class="card-img-top" alt="Site Name Screenshot"><div class="card-body"><h5 class="card-title">Site Name</h5><a href="cpanels-view.html" class="btn btn-primary">View Project <i class="fas fa-eye fa-fw"></i></a></div></div>';
+
+function appendProjects(count, elementId) {
+  var projectCardsHtml = '';
+  for (let i = 0; i < count; i++) {
+    projectCardsHtml += projectCardHtml;
+  }
+  document.getElementById(elementId).innerHTML(projectCardsHtml);
+}
